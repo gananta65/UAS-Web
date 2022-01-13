b2d9e071b8297ee4d03d8816ca504749b5f16c05
gananta65 <gananta65@gmail.com>
2022-01-13 19:17:40 +0800

    membuat dashboard

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <TableTemplate>
-    <template v-slot:title> Attendances Today </template>
+    <template v-slot:title> Hi Admin! </template>
     <template v-slot:search>
       <label for="cari" class="form-label">Search by ID:</label>
       <input
@@ -11,50 +11,6 @@
         name="cari"
         id="cari"
       />
-    </template>
-    <template v-slot:table>
-      <table id="" class="table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Shift</th>
-            <th>Status</th>
-            <th>Reason</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="res in result" :key="res.id">
-            <td>{{ res.id_karyawan }}</td>
-            <td>{{ res.nama_karyawan }}</td>
-            <td>{{ res.shift }}</td>
-            <td>{{ res.status }}</td>
-            <td>
-              <input
-                :disabled="res.status == 'On Time'"
-                type="text"
-                class="form-control"
-                name="reason"
-                v-model="res.reason"
-                id="reason"
-              />
-            </td>
-            <td>
-              <button
-                :disabled="res.status == 'On Time'"
-                @click="
-                  res.tanggal = dateFormat(res.tanggal);
-                  editConfirm(res.id_absensi, res);
-                "
-                class="btn btn-outline-primary"
-              >
-                Edit
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </template>
   </TableTemplate>
 </template>
@@ -71,69 +27,7 @@ export default {
       search: null,
     };
   },
-  methods: {
-    editConfirm(id, data) {
-      console.log("berhasil");
-      swal({
-        title: "Are you sure?",
-        text: "",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      }).then((willDelete) => {
-        if (willDelete) {
-          this.postReason(id, data);
-        }
-      });
-    },
-    checkSearch() {
-      if (this.search == "") {
-        this.getAllData();
-      }
-    },
-    async getAllData() {
-      try {
-        const res = await AttendanceServices.getToday();
-
-        this.result = res.data;
-
-        console.log(this.result);
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    async getByTitle() {
-      try {
-        const res = await AttendanceServices.searchById(this.search);
-
-        this.result = res.data;
-
-        console.log(this.result);
-      } catch (err) {
-        this.getAllData();
-      }
-    },
-    async postReason(id, data) {
-      if (this.result) {
-        try {
-          const res = await AttendanceServices.update(id, data);
-          if (res) {
-            this.inputSuccess();
-            this.$router.push("/employees");
-            console.log("success!");
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    },
-    dateFormat(tanggal) {
-      let p = new Date(tanggal);
-      p.setDate(p.getDate() + 1);
-      let tgl = p.toISOString().slice(0, 10);
-      return tgl;
-    },
-  },
+  methods: {},
 
   mounted() {
     this.getAllData();
